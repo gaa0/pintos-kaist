a261d3beebc54932a06c3304a61c840b5d29dc07
jungbumwoo <jungbumwoo@naver.com>
2021-10-02 10:19:02 +0000

    priority part 1

--- threads/thread.c
+++ threads/thread.c
@@ -66,6 +66,8 @@ static tid_t allocate_tid (void);
 // sleep 상태의 스레드들의 리스트를 따로 관리
 static struct list sleep_list;
 
+bool thread_compare_priority (struct list_elem *l, struct list_elem *s, void *aux UNUSED);
+
 /* Returns true if T appears to point to a valid thread. */
 #define is_thread(t) ((t) != NULL && (t)->magic == THREAD_MAGIC)
 
@@ -214,6 +216,7 @@ thread_create (const char *name, int priority,
 
 	/* Add to run queue. */
 	thread_unblock (t);
+	thread_test_preemption(); // 새로 들어오면 현재 돌아가는 것과 우선순위 비교하여 바꿔줄 수 있는 함수
 
 	return tid;
 }
@@ -248,7 +251,9 @@ thread_unblock (struct thread *t) {
 
 	old_level = intr_disable ();
 	ASSERT (t->status == THREAD_BLOCKED);
-	list_push_back (&ready_list, &t->elem);
+	// list_push_back (&ready_list, &t->elem);
+	list_insert_ordered(&ready_list, &t->elem, thread_compare_priority, 0); // 우선순위 고려하여 리스트에 넣어줌
+
 	t->status = THREAD_READY;
 	intr_set_level (old_level);
 }
@@ -311,7 +316,8 @@ thread_yield (void) {
 
 	old_level = intr_disable ();
 	if (curr != idle_thread)
-		list_push_back (&ready_list, &curr->elem);
+		// list_push_back (&ready_list, &curr->elem);
+		list_insert_ordered(&ready_list, &curr->elem, thread_compare_priority, 0); // 우선순위 고려하여 리스트에 넣어줌
 	do_schedule (THREAD_READY);
 	intr_set_level (old_level);
 }
@@ -320,6 +326,7 @@ thread_yield (void) {
 void
 thread_set_priority (int new_priority) {
 	thread_current ()->priority = new_priority;
+	thread_test_preemption();
 }
 
 /* Returns the current thread's priority. */
@@ -628,4 +635,17 @@ void thread_awake(int64_t ticks)
 			e = list_next(e);
 		}
 	}
+}
+
+bool thread_compare_priority (struct list_elem *l, struct list_elem *s, void *aux UNUSED)
+{
+    return list_entry (l, struct thread, elem)->priority
+         > list_entry (s, struct thread, elem)->priority;
+}
+
+void thread_test_preemption(void)
+{
+	if (!list_empty(&ready_list) && thread_current()->priority <
+	 list_entry(list_front(&ready_list), struct thread, elem)->priority)
+		thread_yield();
 }
